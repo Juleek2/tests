6300056caecebae519d8e5bd60ad7b6edf6f61e2
Ivan Nomokonov <nomokonovi@vtb.ru>
2025-11-19 17:24:46 +0300

    actions

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,53 @@ import "./App.css";
 import type { Data } from "./types";
 import { Test } from "./components/test";
 
+const tests = [
+  "ДефицитДГЭА.json",
+  "ПризнакиСтарения.json",
+  "КлимактерическийСиндром.json",
+  "ДефицитЖелеза.json",
+  "ДефицитГормонаСна.json",
+  "СостояниеЩитовиднойЖелезы.json",
+  "ДефицитМагния.json",
+  "ДефицитПрогестерона.json",
+  "СахарныйДиабед.json",
+  "ПредстательнаяЖелеза.json",
+];
+
 function App() {
   const [data, setData] = useState<Data>();
+  const [active, setActive] = useState<string>();
 
   useEffect(() => {
-    fetch("/data/КлимактерическийСиндром.json")
+    if (!active) {
+      setData(undefined);
+      return;
+    }
+    fetch(`/data/${active}`)
       .then((d) => d.json())
       .then((d) => setData(d));
-  }, []);
+  }, [active]);
 
   if (!data) {
-    return null;
+    return (
+      <div className="list">
+        {tests.map((t) => (
+          <a href="javascript:;" key={t} onClick={() => setActive(t)}>
+            {t}
+          </a>
+        ))}
+      </div>
+    );
   }
 
-  return <Test data={data} />;
+  return (
+    <div>
+      <div className="actions">
+        <button onClick={() => setActive(undefined)}>Назад</button>
+      </div>
+      <Test data={data} />
+    </div>
+  );
 }
 
 export default App;
